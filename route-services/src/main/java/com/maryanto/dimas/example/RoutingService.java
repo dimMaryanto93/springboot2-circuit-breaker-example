c07389383a56102a0670fbfc3d3b523b455e5dd7
dimasm93 <software.dimas_m@icloud.com>
2020-02-14 14:38:27 +0700

    enabled route service

---
+++ route-services/src/main/java/com/maryanto/dimas/example/RoutingService.java
@@ -0,0 +1,11 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingService {
+    public static void main(String[] args) {
+        SpringApplication.run(RoutingService.class, args);
+    }
+}
